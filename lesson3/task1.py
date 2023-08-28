c72c55b097626c2e738cc6de01283e275aa76299
Vladimir Stepankin <vladimirstepankin@mail.ru>
2023-08-28 16:07:34 +0300

    Добавил ДЗ3

---
+++ lesson3/task1.py
@@ -0,0 +1,11 @@
+# Задача 1
+
+
+my_list = (1, 4, 2, 44, 1, 26, 3, 3, 1, 44, 87)
+
+new_list = []
+for el in my_list:
+    if my_list.count(el) > 1 and new_list.count(el) == 0:
+        new_list.append(el)
+print(my_list)
+print(new_list)
---
+++ lesson3/task3.py
@@ -0,0 +1,46 @@
+# Задача 3
+
+import itertools
+
+things = {
+    "Палатка": 2.5,
+    "Спальник": 1.5,
+    "Пенка": 0.5,
+    "Еда": 1.0,
+    "Перчатки": 0.2,
+    "Фонарик": 0.3,
+    "Котелок": 1.0,
+    "Ботинки": 2.0,
+    "Вода": 3.0
+}
+
+MAX_WEIGHT = 7.0
+
+
+def all_combinations(items):
+    all_combinations = []
+    for i in range(1, len(items) + 1):
+        combinations = itertools.combinations(items, i)
+        all_combinations.extend(combinations)
+    return all_combinations
+
+
+def valid_combinations(list, max_weight):
+    valid_combinations = []
+    for combination in list:
+        total_weight = sum(item[1] for item in combination)
+        if total_weight <= max_weight:
+            valid_combinations.append(combination)
+    return valid_combinations
+
+
+valid_combinations = valid_combinations(all_combinations(things.items()), MAX_WEIGHT)
+
+for combination in valid_combinations:
+    print("Вариант комплектации:")
+    total_weight = 0
+    for item in combination:
+        total_weight = total_weight + item[1]
+        print(f"{item[0]}")
+    print(f"Общий вес: {total_weight}")
+    print()
