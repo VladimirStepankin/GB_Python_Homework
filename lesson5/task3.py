82f86a23239a32211ed51958a524578b335763e7
Vladimir Stepankin <vladimirstepankin@mail.ru>
2023-08-31 11:25:29 +0300

    Добавил ДЗ5

---
+++ lesson5/task3.py
@@ -0,0 +1,14 @@
+# Задача 3.
+
+
+def gen_fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+number_fib = gen_fibonacci()
+for _ in range(15):
+    number = next(number_fib)
+    print(number, end=' ')
